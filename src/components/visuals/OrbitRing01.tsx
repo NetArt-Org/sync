e1c65740b508ca5594be22809497af2ea9593e3d
Kavita Rawat <kavitarawat@Kavitas-MacBook-Air.local>
2026-07-20 21:47:46 +0530

    basic-start project

--- src/components/visuals/OrbitRing01.tsx
+++
@@ -1,5 +0,0 @@
-import { OrbitRingBase } from "./OrbitRingBase";
-
-export function OrbitRing01() {
-  return <OrbitRingBase sizePercent={52} duration={34} direction={1} opacity={0.9} />;
-}
--- src/components/visuals/OrbitRing03.tsx
+++
@@ -1,5 +0,0 @@
-import { OrbitRingBase } from "./OrbitRingBase";
-
-export function OrbitRing03() {
-  return <OrbitRingBase sizePercent={84} duration={64} direction={1} opacity={0.45} />;
-}
